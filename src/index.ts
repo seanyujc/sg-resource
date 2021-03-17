73cd2119d71da329fb09405e49db2ce528d0ffa2
yjc <yujincheng@yaoyanshe.com>
2021-03-17 14:48:09 +0800

    add confing

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-
 import { ConfigAdapter } from "./config-adapter";
 import { createSingletonObject, Autowired } from "./decorator";
 import { IInterceptorsOptions, ProxyHttp } from "./proxy-http";
@@ -7,21 +6,64 @@ export interface IHost {
   url: string;
   cors?: boolean;
 }
+/**
+ * 某一个站点配置
+ */
 export interface ISite {
+  /**
+   * 远端服务器配置
+   */
   remote: {
+    /**
+     * 服务器地址列表
+     */
     hosts: Record<string, IHost | string>;
+    /**
+     * 默认协议，服务器地址不设置可继承该设置
+     */
     protocol?: string;
   };
+  /**
+   * 本地服务器配置
+   */
   local: {
+    /**
+     * 协议
+     */
     protocol?: string;
+    /**
+     * 主机域名或ip地址
+     */
     hostname?: string;
+    /**
+     * 端口占用
+     */
     port?: number;
+    /**
+     * 发布路径
+     */
     publicPath: string;
+    /**
+     * 附件路径，可以是本地服务目录，或远程地址
+     */
     assetsPath?: string;
   };
+  /**
+   * 第三方登录入口
+   */
   entrance?: string;
+  /**
+   * 统计服务地址
+   */
   sensor?: string;
+  /**
+   * 接口加密盐
+   */
+  salt?: string;
 }
+/**
+ * 四个环境站点配置
+ */
 export interface ISiteConfig {
   DEV: ISite;
   TEST: ISite;
