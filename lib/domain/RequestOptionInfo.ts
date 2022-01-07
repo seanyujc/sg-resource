217ace59e2f47ca307c3a47e9f6374587bbb0af2
YuJincheng <yujincheng@yaoyanshe.com>
2022-01-07 18:14:08 +0800

    option

--- lib/domain/RequestOptionInfo.ts
+++ lib/domain/RequestOptionInfo.ts
@@ -1,5 +1,5 @@
 export class RequestOptionInfo {
   headers?: any;
-  cancelLevel: 0 | 1 | 2 | 3 | 4 | 5 | 6 = 0;
-  overCancelUpLevel = false
+  cancelLevel?: 0 | 1 | 2 | 3 | 4 | 5 | 6 = 0;
+  overCancelUpLevel? = false
 }
